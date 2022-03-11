d7209df5d70b11323108d18234554cf8a5b27ee8
Isaque Lima Leite <30935061+IsacLima@users.noreply.github.com>
2022-03-11 02:12:09 +0000

    feat: add initial movement

--- script/script.js
+++ script/script.js
@@ -1,6 +1,19 @@
 const section = document.getElementsByClassName("section__img")
 const tooltip = document.getElementsByClassName("tooltip")
 const back = document.getElementsByClassName("back")
+const left = document.getElementsByClassName("footer__left")
+const right = document.getElementsByClassName("footer__right")
+const carousel = document.getElementsByClassName("carousel__content")
+let distanceRightTop = "100"
+
+
+Array.from(left).forEach(item=>{
+	item.addEventListener("click", openTooltip)
+})
+
+Array.from(right).forEach(item=>{
+	item.addEventListener("click", moveRight)
+})
 
 Array.from(section).forEach(item=>{
 	item.addEventListener("click", openTooltip)
@@ -17,4 +30,10 @@ back[0].addEventListener("click", ()=>{
 function openTooltip(){
 	tooltip[0].style.animation = "scale .35s"
 	tooltip[0].style.display = "block"
+}
+
+function moveRight(){
+	if(this.attributes.value.value == "top"){
+		carousel[0].style.transform = "translateX("	+ distanceRightTop + "px)"
+	}
 }
